5c747d9eaaa26097af8782486da8fabae18856af
zugamifk <zugamifk@gmail.com>
2022-05-15 17:24:22 -0300

    packagedeskitem

--- Assets/Scripts/View/Desk/Items/DeskItem.cs
+++ Assets/Scripts/View/Desk/Items/DeskItem.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Identifiable))]
 public class DeskItem : MonoBehaviour, IView<IItemModel>
 {
     public string Name;
-    IItemModel _model;
 
     public void InitializeFromModel(IItemModel model)
     {
-        _model = model;
+        var identifiable = GetComponent<Identifiable>();
+        identifiable.Id = model.Id;
     }
 }
---
+++ Assets/Scripts/View/Desk/Items/PackageDeskItem.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Clickable))]
+[RequireComponent(typeof(Identifiable))]
+public class PackageDeskItem : MonoBehaviour
+{
+    private void Start()
+    {
+        var clickable = GetComponent<Clickable>();
+        clickable.Clicked += ClickAction;
+    }
+
+    void ClickAction(int button)
+    {
+        Debug.Log("Clicked " + this);
+    }
+}
