6f4bddb798aa51b3cbc591391f2458debe6ae3b3
zugamifk <zugamifk@gmail.com>
2022-05-23 13:25:37 -0300

    blood

---
+++ Assets/Scripts/Model/Character/Health/Tests/BloodTests.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Health.Tests
+{
+    public class BloodTests
+    {
+        [Test]
+        public void NewBlood_EmptyConstructor_NoOxygen()
+        {
+            var blood = new BloodModel();
+            Assert.AreEqual((float)blood.OxygenLevel, 0);
+        }
+    }
+}
