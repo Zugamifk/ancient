ac65b299ec2065f31ceccfd0ef2b74ccc6325328
zugamifk <zugamifk@gmail.com>
2022-05-24 00:49:45 -0300

    tower and firing behaviour

---
+++ Assets/Scripts/Subsystems/TowerDefense/Behaviours/TowerBehaviours/TowerBehaviour.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TowerDefense.Models;
+
+namespace TowerDefense.Behaviours
+{
+    public class TowerBehaviour
+    {
+        FiringBehaviour _firing = new();
+
+        public void UpdateFiringState(Tower tower)
+        {
+
+        }
+    }
+}
