9fdb041417aad1863f7625024e2b1d084cc0c245
zugamifk <zugamifk@gmail.com>
2022-05-27 16:24:10 -0300

    connect souces

--- Assets/Scripts/Subsystems/Health/Parts/BloodVessel.cs
+++ Assets/Scripts/Subsystems/Health/Parts/BloodVessel.cs
@@ -5,13 +5,23 @@ using Fluids;
 
 namespace Health
 {
-    public class BloodVessel
+    public class BloodVessel : IBloodCirculator
     {
         public FluidVolume Volume { get; }
         public Blood BloodContents => Volume.Fluids.GetFluid<Blood>();
+
+        public ISet<IBloodCirculator> Sources { get; } = new HashSet<IBloodCirculator>();
+
+        public ISet<IBloodCirculator> Sinks { get; } = new HashSet<IBloodCirculator>();
+
         public BloodVessel(float capacity)
         {
             Volume = new FluidVolume(capacity);
+        }
+
+        public void ConnectSource(IBloodCirculator source)
+        {
+            Sources.Add(source);
         }
     }
 }
--- Assets/Scripts/Subsystems/Health/Tests/BloodVesselTests.cs
+++ Assets/Scripts/Subsystems/Health/Tests/BloodVesselTests.cs
@@ -9,17 +9,44 @@ namespace Health.Tests
     public class BloodVesselTests
     {
         [Test]
-        public void Constructor_HasVolume()
+        public void New_HasVolume()
         {
             var bloodVessel = new BloodVessel(10);
             Assert.NotNull(bloodVessel.Volume);
         }
 
         [Test]
-        public void Constructor_NoFluid()
+        public void New_NoFluid()
         {
             var bloodVessel = new BloodVessel(10);
             Assert.AreEqual(0, bloodVessel.Volume.Fluids.Measure.Value);
+        }
+
+        [Test]
+        public void New_SourcesEmpty()
+        {
+            var bloodVessel = new BloodVessel(10);
+            Assert.Zero(bloodVessel.Sources.Count);
+        }
+
+        [Test]
+        public void New_SinksEmpty()
+        {
+            var bloodVessel = new BloodVessel(10);
+            Assert.Zero(bloodVessel.Sinks.Count);
+        }
+
+        [Test]
+        public void ConnectSource_AddsToSources()
+        {
+            var bloodVessel = new BloodVessel(10);
+            var source = new BloodVessel(10);
+
+            Assert.IsFalse(bloodVessel.Sources.Contains(source));
+
+            bloodVessel.ConnectSource(source);
+
+            Assert.IsTrue(bloodVessel.Sources.Contains(source));
         }
     }
 }
