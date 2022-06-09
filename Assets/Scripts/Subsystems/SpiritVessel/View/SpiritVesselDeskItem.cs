e30e53d76056fb55374c366d7a6c35f68f5ab671
zugamifk <zugamifk@gmail.com>
2022-06-09 20:40:32 -0300

    spirit vessel prefab

---
+++ Assets/Scripts/Subsystems/SpiritVessel/View/SpiritVesselDeskItem.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpiritVessel.View
+{
+    public class SpiritVesselDeskItem : MonoBehaviour
+    {
+        [SerializeField]
+        SpriteRenderer _mainSealSprite;
+        [SerializeField]
+        SpriteRenderer[] _redSealSprites;
+        [SerializeField]
+        SpriteRenderer[] _whiteSealSprites;
+    }
+}
