b470d22593d478b5c9c42e9bf40520d539af6b63
zugamifk <zugamifk@gmail.com>
2022-06-27 21:07:51 -0300

    collect enemies in cloud radius

--- Assets/Scripts/Subsystems/SpiritVessel/View/Cloud.cs
+++ Assets/Scripts/Subsystems/SpiritVessel/View/Cloud.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using SpiritVessel.ViewModel;
+using System;
 
 namespace SpiritVessel.View
 {
@@ -9,7 +10,7 @@ namespace SpiritVessel.View
     public class Cloud : MonoBehaviour, IView<ILightningSkillCloudModel>
     {
         Identifiable _identifiable;
-
+        HashSet<Guid> _containedSpirits = new();
         public void InitializeFromModel(ILightningSkillCloudModel model)
         {
             _identifiable.Id = model.Id;
@@ -23,6 +24,25 @@ namespace SpiritVessel.View
         void Update()
         {
             var model = Game.Model.GetModel<ISpiritVesselModel>().LightningSkill.Clouds.GetItem(_identifiable.Id);
+            Debug.Log(_containedSpirits.Count);
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            var enemy = collision.gameObject.GetComponent<Spirit>();
+            if (enemy != null)
+            {
+                _containedSpirits.Add(enemy.Id);
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            var enemy = collision.gameObject.GetComponent<Spirit>();
+            if (enemy != null)
+            {
+                _containedSpirits.Remove(enemy.Id);
+            }
         }
     }
 }
