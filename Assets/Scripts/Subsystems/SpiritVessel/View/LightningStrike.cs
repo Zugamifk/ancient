c5ea29243341581bb634024493a8dbbed8afb122
zugamifk <zugamifk@gmail.com>
2022-06-30 21:24:49 -0300

    draw lightning chains

--- Assets/Scripts/Subsystems/SpiritVessel/View/LightningStrike.cs
+++ Assets/Scripts/Subsystems/SpiritVessel/View/LightningStrike.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using SpiritVessel.ViewModel;
+using SpiritVessel.Data;
 
 namespace SpiritVessel.View
 {
@@ -39,7 +40,7 @@ namespace SpiritVessel.View
                 var inRangeCount = Physics2D.OverlapCircleNonAlloc(origin, lightning.ChainRadius, _colliderBuffer);
                 if (inRangeCount == 0)
                 {
-                    return;
+                    break;
                 }
 
                 for (int j = 0; j < 100; j++)
@@ -48,7 +49,7 @@ namespace SpiritVessel.View
                     spirit = target.gameObject.GetComponent<Spirit>();
                     if (spirit == null)
                     {
-                        return;
+                        continue;
                     }
 
                     if (!_chainTargets.Contains(spirit))
@@ -57,14 +58,14 @@ namespace SpiritVessel.View
                     }
                 }
 
-                if (_chainTargets.Contains(spirit))
+                if (spirit == null || _chainTargets.Contains(spirit))
                 {
-                    return;
+                    break;
                 }
 
                 HitSpirit(spirit);
 
-                Debug.DrawLine(origin, spirit.transform.position);
+                ShowChain(origin, spirit.transform.position);
 
                 origin = spirit.transform.position;
             }
@@ -75,6 +76,19 @@ namespace SpiritVessel.View
             Game.Do(new DamageSpiritCommand(spirit.Id, 2));
             VfxService.Play("LightningHit", spirit.transform.position, spirit.transform.parent);
             _chainTargets.Add(spirit);
+        }
+
+        void ShowChain(Vector3 start, Vector3 end)
+        {
+            Debug.DrawLine(start, end);
+            var prefabs = DataService.GetData<SkillPrefabs>();
+            var linePrefab = prefabs.GetPrefab("LightningChain");
+            var line = Instantiate(linePrefab, transform);
+
+            line.transform.position = start;
+            var dir = end - start;
+            line.transform.rotation = Quaternion.AngleAxis(Mathf.Rad2Deg * Mathf.Atan2(dir.y, dir.x), Vector3.forward);
+            line.transform.localScale = new Vector3(dir.magnitude, 1, 1);
         }
     }
 }
