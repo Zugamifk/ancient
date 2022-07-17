a7fdbbfe2222a845fbf8459c223faa755d99b9be
zugamifk <zugamifk@gmail.com>
2022-07-17 01:20:31 -0300

    map editor window, terrain

---
+++ Assets/Scripts/UnityModules/MapEditor/MapEditorWindow.cs
@@ -0,0 +1,28 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+
+
+public class MapEditorWindow : EditorWindow
+{
+    [MenuItem("Window/UI Toolkit/MapEditorWindow")]
+    public static void ShowExample()
+    {
+        MapEditorWindow wnd = GetWindow<MapEditorWindow>();
+        wnd.titleContent = new GUIContent("MapEditorWindow");
+    }
+
+    public void CreateGUI()
+    {
+        VisualElement root = rootVisualElement;
+
+        var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Scripts/UnityModules/MapEditor/MapEditorWindow.uxml");
+        var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Scripts/UnityModules/MapEditor/MapEditorWindow.uss");
+
+        VisualElement instance = visualTree.Instantiate();
+        instance.styleSheets.Add(styleSheet);
+
+        root.Add(instance);
+    }
+}
