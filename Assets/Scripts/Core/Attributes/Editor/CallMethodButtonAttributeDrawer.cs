af8cc644dbf855c56ed868fac925bb7192e7eff8
zugamifk <zugamifk@gmail.com>
2022-07-17 11:01:59 -0300

    cube generator, generator button

--- Assets/Scripts/Core/Attributes/Editor/CallMethodButtonAttributeDrawer.cs
+++ Assets/Scripts/Core/Attributes/Editor/CallMethodButtonAttributeDrawer.cs
@@ -2,27 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System;
+using System.Reflection;
 
 [CustomPropertyDrawer(typeof(CallMethodButtonAttribute))]
-public class CallMethodButtonAttributeDrawer : DecoratorDrawer
+public class CallMethodButtonAttributeDrawer : PropertyDrawer
 {
     CallMethodButtonAttribute button => attribute as CallMethodButtonAttribute;
 
-    public override float GetHeight()
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
     {
-        return 16;
+        return 36;
     }
 
-    public override void OnGUI(Rect position)
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
-        if(GUI.Button(position, "Generate Mesh"))
+        var rect = position;
+        rect.height = 16;
+
+        GUI.enabled = false;
+        EditorGUI.PropertyField(rect, property);
+        GUI.enabled = true;
+
+        rect.y += 20;
+        if (GUI.Button(rect, "Generate Mesh"))
         {
-            Call();
+            Call(property);
         }
     }
 
-    void Call()
+    void Call(SerializedProperty property)
     {
         Debug.Log("Pressed");
+        var obj = property.serializedObject;
+        if (obj.targetObject is MonoBehaviour)
+        {
+            Type type = obj.targetObject.GetType();
+            MethodInfo info = type.GetMethod(button.MethodName, BindingFlags.NonPublic | BindingFlags.FlattenHierarchy | BindingFlags.Instance | BindingFlags.Public);
+            if (info != null)
+            {
+                fieldInfo.SetValue(obj.targetObject, info.Invoke(obj.targetObject, null));
+            }
+            else
+            {
+                Debug.LogErrorFormat("Method \"{0}\" not a member of type \"{1}\"", button.MethodName, type.Name);
+            }
+        }
     }
 }
--- Assets/Scripts/UnityModules/MeshGenerator/PrimitiveMeshGenerator.cs
+++ Assets/Scripts/UnityModules/MeshGenerator/PrimitiveMeshGenerator.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace MeshGenerator
@@ -7,13 +5,67 @@ namespace MeshGenerator
     public class PrimitiveMeshGenerator : MonoBehaviour
     {
         [CallMethodButton("Generate", "Generate Mesh")]
-        
+        [SerializeField]
+        Mesh _mesh;
         [SerializeField]
         EPrimitiveMeshType _meshType;
+        [SerializeField]
+        float _size = 1;
 
         public Mesh Generate()
         {
-            return default;
+            Mesh m = default;
+            switch (_meshType)
+            {
+                case EPrimitiveMeshType.Cube:
+                    m = GenerateCube();
+                    break;
+                default:
+                    break;
+            }
+
+            var mf = GetComponent<MeshFilter>();
+            mf.mesh = m;
+            return m;
+        }
+
+        Mesh GenerateCube()
+        {
+            var m = new Mesh();
+            m.name = "Cube";
+
+            var r = _size / 2;
+            var v = new Vector3[]
+            {
+                new Vector3(-r,r,-r), new Vector3(-r,r,r), new Vector3(r,r,r), new Vector3(r,r,-r),
+                new Vector3(-r,-r,-r), new Vector3(-r,-r,r), new Vector3(-r,r,r), new Vector3(-r,r,-r),
+                new Vector3(r,-r,r), new Vector3(r,r,r), new Vector3(-r,r,r), new Vector3(-r,-r,r),
+                new Vector3(r,-r,-r), new Vector3(r,r,-r), new Vector3(r,r,r), new Vector3(r,-r,r),
+                new Vector3(-r,-r,-r), new Vector3(-r,r,-r), new Vector3(r,r,-r), new Vector3(r,-r,-r),
+                new Vector3(-r,-r,-r), new Vector3(r,-r,-r), new Vector3(r,-r,r), new Vector3(-r,-r,r),
+            };
+            var n = new Vector3[]
+            {
+                Vector3.up, Vector3.up, Vector3.up, Vector3.up,
+                Vector3.left,Vector3.left,Vector3.left,Vector3.left,
+                Vector3.forward,Vector3.forward,Vector3.forward,Vector3.forward,
+                Vector3.right,Vector3.right,Vector3.right,Vector3.right,
+                Vector3.back,Vector3.back,Vector3.back,Vector3.back,
+                Vector3.down,Vector3.down,Vector3.down,Vector3.down,
+            };
+            var t = new int[]
+            {
+                0, 1, 2, 0, 2, 3,
+                4, 5, 6, 4, 6, 7,
+                8, 9, 10, 8, 10, 11,
+                12, 13, 14, 12, 14, 15,
+                16, 17, 18, 16, 18, 19,
+                20, 21, 22, 20, 22, 23
+            };
+            m.vertices = v;
+            m.normals = n;
+            m.triangles = t;
+            return m;
         }
     }
 }
