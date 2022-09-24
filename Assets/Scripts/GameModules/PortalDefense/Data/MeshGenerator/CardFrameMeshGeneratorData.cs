6f19f304df293e607be9ada297ed7b45d33028c9
zugamifk <zugamifk@gmail.com>
2022-09-23 23:25:37 -0300

    card mesh generator

--- Assets/Scripts/GameModules/PortalDefense/Data/MeshGenerator/CardFrameMeshGeneratorData.cs
+++ Assets/Scripts/GameModules/PortalDefense/Data/MeshGenerator/CardFrameMeshGeneratorData.cs
@@ -7,5 +7,10 @@ namespace PortalDefense.Data
     public class CardFrameMeshGeneratorData : ScriptableObject
     {
         public Vector2 BaseDimensions;
+        [Min(0)]
+        public float BorderWidth;
+        [Range(0,1)]
+        public float DividerPosition;
+        public float DividerWidth;
     }
 }
--- Assets/Scripts/GameModules/PortalDefense/Services/MeshGenerators/CardFrameMeshGenerator.cs
+++ Assets/Scripts/GameModules/PortalDefense/Services/MeshGenerators/CardFrameMeshGenerator.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using MeshGenerator;
 using MeshGenerator.Wireframe;
 using PortalDefense.Data;
+using System;
 
 namespace PortalDefense.Services
 {
@@ -14,18 +15,56 @@ namespace PortalDefense.Services
         {
             Wireframe = new();
 
-            var w = Data.BaseDimensions.x / 2;
-            var h = Data.BaseDimensions.y / 2;
-            var p0 = new Point(-w, -h, 0);
-            var p1 = new Point(-w, h, 0);
-            var p2 = new Point(w, h, 0);
-            var p3 = new Point(w, -h, 0);
+            Func<float> w = () => Data.BaseDimensions.x / 2;
+            Func<float> h = () => Data.BaseDimensions.y / 2;
+            var p0 = new DynamicPoint(() => new Vector3(-w(), -h(), 0));
+            var p1 = new DynamicPoint(() => new Vector3(-w(), h(), 0));
+            var p2 = new DynamicPoint(() => new Vector3(w(), h(), 0));
+            var p3 = new DynamicPoint(() => new Vector3(w(), -h(), 0));
 
             Wireframe.ConnectLoop(p0, p1, p2, p3);
+
+            Func<float> bw = () => w() + Data.BorderWidth;
+            Func<float> bh = () => h() + Data.BorderWidth;
+            var b0 = new DynamicPoint(() => new Vector3(-bw(), -bh(), 0));
+            var b1 = new DynamicPoint(() => new Vector3(-bw(), bh(), 0));
+            var b2 = new DynamicPoint(() => new Vector3(bw(), bh(), 0));
+            var b3 = new DynamicPoint(() => new Vector3(bw(), -bh(), 0));
+            Wireframe.ConnectLoop(b0, b1, b2, b3);
+
+            Func<float> dw = () => Data.DividerWidth / 2;
+            Func<float> d = ()=>Mathf.Lerp(dw(), Data.BaseDimensions.y-dw(), Data.DividerPosition);
+            var d0 = new DynamicPoint(() => p0.Position + new Vector3(0, d() - dw(), 0));
+            var d1 = new DynamicPoint(() => p3.Position + new Vector3(0, d() - dw(), 0));
+            Wireframe.Connect(d0, d1);
+            var d2 = new DynamicPoint(() => p0.Position + new Vector3(0, d() + dw(), 0));
+            var d3 = new DynamicPoint(() => p3.Position + new Vector3(0, d() + dw(), 0));
+            Wireframe.Connect(d2, d3);
         }
 
         public override void Generate(MeshBuilder builder)
         {
+            builder.SetColor(Color.white);
+
+            var w = Data.BaseDimensions.x / 2;
+            var h = Data.BaseDimensions.y / 2;
+            var p0 = new Vector3(-w, -h, 0);
+            var p1 = new Vector3(-w, h, 0);
+            var p2 = new Vector3(w, h, 0);
+            var p3 = new Vector3(w, -h, 0);
+            builder.AddQuad(p0, p1, p2, p3);
+
+            var bw = w + Data.BorderWidth;
+            var bh = h + Data.BorderWidth;
+            var b0 = new Vector3(-bw, -bh, 0);
+            var b1 = new Vector3(-bw, bh, 0);
+            var b2 = new Vector3(bw, bh, 0);
+            var b3 = new Vector3(bw, -bh, 0);
+
+            builder.AddQuad(p0, b0, b1, p1);
+            builder.AddQuad(p1, b1, b2, p2);
+            builder.AddQuad(p2, b2, b3, p3);
+            builder.AddQuad(p3, b3, b0, p0);
         }
 
         protected override CardFrameMeshGeneratorData LoadData() => DataService.GetData<PortalDefenseMeshGeneratorDataCollection>().CardFrame;
